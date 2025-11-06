f28ecb11eab9eef8f466d24838dc0474aa375b29
Shui <zelongshui@meridianinno.com>
2025-11-06 13:29:12 +0800

    feat(core): Export Senxor, FIELDS, and REGISTERS from core module

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,7 @@ export * as consts from "./consts";
 export * as senxorProcessor from "./processors";
 export * from "./error";
 export * from "./types";
+
+export { Senxor } from "./senxor";
+export { FIELDS } from "./fields";
+export { REGISTERS } from "./registers";
